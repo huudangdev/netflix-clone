8e70365845e09fcaabd2cf330dd3d47975a38f5e
huudangdev <nguyentranhuudang.blog@gmail.com>
2019-07-20 15:46:34 +0700

    Updates

---
+++ precache-manifest.079d4c959409e6334be574e5c9c642f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85c3da272153495ee2d60f99b3cf197",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "d4384a1a09324392502f",
+    "url": "/netflix-clone/static/css/main.d43e914e.chunk.css"
+  },
+  {
+    "revision": "ba714fb5708389851adb",
+    "url": "/netflix-clone/static/js/2.764849d0.chunk.js"
+  },
+  {
+    "revision": "d4384a1a09324392502f",
+    "url": "/netflix-clone/static/js/main.9ae928ba.chunk.js"
+  },
+  {
+    "revision": "ded34aa85da1aff6ad44",
+    "url": "/netflix-clone/static/js/runtime~main.21c42e60.js"
+  },
+  {
+    "revision": "dc2411c98b969c2d71b0920ae3e2d9d8",
+    "url": "/netflix-clone/static/media/bg.dc2411c9.jpg"
+  },
+  {
+    "revision": "35f3a5aec6a2026d1f96de322219136d",
+    "url": "/netflix-clone/static/media/logo.35f3a5ae.svg"
+  }
+]);
